8b1c9aa76e40e7f484f2f41c59169d327f5dd172
mquig42 <darthquigley@gmail.com>
2016-06-11 22:15:14 -0400

    Added bluetooth
    Added some code to test sending/receiving data through bluetooth

--- BluetoothRobot.ino
+++ BluetoothRobot.ino
@@ -9,6 +9,7 @@
 
 RobotMotor *leftMotor = new RobotMotor(7, 8, 6);
 RobotMotor *rightMotor = new RobotMotor(9, 10, 11);
+char btIn[10];
 
 void processLeftTick()
 {
@@ -24,15 +25,25 @@ void setup()
 {
     pinMode(2, INPUT);
     pinMode(3, INPUT);
+
     attachInterrupt(digitalPinToInterrupt(2), processLeftTick, FALLING);
     attachInterrupt(digitalPinToInterrupt(3), processRightTick, FALLING);
 
-    //For now, just tell the left motor to move forward 20 ticks as a test
-    leftMotor->move(-20, 255);
+    //The Bluetooth module is connected through the UART, and works like a serial port
+    Serial.begin(9600);
+    //Because the motors won't be running while the program waits for input, we can
+    //use blocking I/O functions. Set timeout to 1 hour.
+    Serial.setTimeout(3600000);
+
+    //Motor movements commented out for bluetooth testing
+    //rightMotor->move(-40, 255);
 }
 
 void loop()
 {
+    Serial.readBytesUntil(0x0A, btIn, 10);
+    Serial.print(btIn);
+    
     leftMotor->refresh();
     rightMotor->refresh();
 }
