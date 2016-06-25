cf18487a1b13e0bf3e69ea633b339c716d0bbc82
mquig42 <darthquigley@gmail.com>
2016-06-25 03:33:28 -0400

    Added preliminary command parser
    The robot will be controlled by a basic interpreted programming
    language. For the moment, it will accept a sequence of up to 16
    commands. CLEAR; is implemented, RUN; will currently just echo the
    stored commands through the serial port (or bluetooth, if connected).
    Other commands not implemented yet.

--- BluetoothRobot.ino
+++ BluetoothRobot.ino
@@ -6,10 +6,18 @@
  * Control code for a small robot, with basic bluetooth driving controls
  ******************************************************************************/
 #include <RobotMotor.h>
+#include <string.h>
+
+#define COMMAND_BUFFER 16           //Number of commands to store
+
+enum state {WAIT, RUN};
+state s = WAIT;
 
 RobotMotor *leftMotor = new RobotMotor(7, 8, 6);
 RobotMotor *rightMotor = new RobotMotor(9, 10, 11);
-char btIn[10];
+
+char commands[COMMAND_BUFFER][11];  //Array of C strings to store program.
+uint8_t pc = 0;                     //program counter
 
 void processLeftTick()
 {
@@ -19,6 +27,37 @@ void processLeftTick()
 void processRightTick()
 {
     rightMotor->processTick();
+}
+
+/** getNextToken
+ *  Used for parsing delimited strings
+ *  Will find the substring beginning at start, and ending before next occurrence of sep
+ *  Substring will be copied to out
+ *  returns index after sep, or 0 if the end of str was reached
+ */
+uint8_t getNextToken(char *str, char sep, uint8_t start, char *out)
+{
+    int i = 0;
+    while((str[i+start])&&(str[i+start] != sep))
+    {
+        out[i] = str[i+start];
+        i++;
+    }
+    if(str[i+start])
+        return i + 1;
+    return 0;
+}
+
+/** clearProgram
+ *  Erases all stored commands and resets pc to 0
+ */
+void clearProgram()
+{
+    for(pc=0;pc<COMMAND_BUFFER;pc++)
+    {
+        commands[pc][0] = '\0';
+    }
+    pc = 0;
 }
 
 void setup()
@@ -34,15 +73,37 @@ void setup()
     //Because the motors won't be running while the program waits for input, we can
     //use blocking I/O functions. Set timeout to 1 hour.
     Serial.setTimeout(3600000);
-
-    //Motor movements commented out for bluetooth testing
-    //rightMotor->move(-40, 255);
 }
 
 void loop()
 {
-    Serial.readBytesUntil(0x0A, btIn, 10);
-    Serial.print(btIn);
+    switch(s)
+    {
+        case WAIT:
+            Serial.readBytesUntil(';', commands[pc], 10);
+            if(strncmp(commands[pc], "RUN", 3) == 0)
+            {
+                pc = 0;
+                s = RUN;
+                break;
+            }
+            if(strncmp(commands[pc], "CLEAR", 5) == 0)
+            {
+                clearProgram();
+                break;
+            }
+            pc++;
+            break;
+        case RUN:
+            if(strncmp(commands[pc], "RUN", 3) == 0)
+            {
+                s = WAIT;
+                break;
+            }
+            Serial.print(commands[pc]);
+            pc++;
+            break;
+    }
     
     leftMotor->refresh();
     rightMotor->refresh();
